7b324eb9f86345b9735889832f8e3501be6bf361
PROFE9B\soldadoj <jsoldado@iesruizgijon.com>
2025-02-13 09:11:33 +0100

    Añadido interpolación de propiedades y métodos

--- proyecto001/src/app/app.component.ts
+++ proyecto001/src/app/app.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto001';
+
+  nombre = "Javier";
+  edad = 51;
+  fumador = false;
+
+  esFumador(){
+    return this.fumador?"Sí es fumador":"No es fumador";
+  }
+
 }
