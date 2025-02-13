8c9a93ba8d9a14057657e1158b00f103c2f86c4c
Javier Soldado <jsoldado@iesruizgijon.com>
2025-02-13 17:27:58 +0100

    Agregado directivas *ngIf y *ngFor en proyecto001

--- proyecto001/src/app/app.component.ts
+++ proyecto001/src/app/app.component.ts
@@ -13,6 +13,14 @@ export class AppComponent {
   edad = 51;
   fumador = false;
 
+  provincias = [
+    {id:1, nombre:"Toledo"},
+    {id:2, nombre:"Ciudad Real"},
+    {id:3, nombre:"Cuenca"},
+    {id:4, nombre:"Guadalajara"},
+    {id:5, nombre:"Albacete"}
+  ];
+
   esFumador(){
     return this.fumador?"Sí es fumador":"No es fumador";
   }
