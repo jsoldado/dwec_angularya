87962c147062b5e515af6de7a633b16a77b1400b
jsoldado <jsoldado@iesruizgijon.com>
2025-02-19 16:10:50 +0100

    Creado proyecto005 con componente countdown

---
+++ proyecto005/src/app/components/countdown/countdown.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-countdown',
+  standalone: false,
+  templateUrl: './countdown.component.html',
+  styleUrl: './countdown.component.css'
+})
+export class CountdownComponent {
+
+}
